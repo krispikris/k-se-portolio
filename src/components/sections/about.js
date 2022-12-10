942a4aec70add091312c61531c38e47961b63769
Kristopher Han <kris@kristopherhan.com>
2022-12-09 16:43:47 -0800

    about me added

--- src/components/sections/about.js
+++ src/components/sections/about.js
@@ -125,7 +125,7 @@ const About = () => {
     sr.reveal(revealContainer.current, srConfig());
   }, []);
 
-  const skills = ['JavaScript (ES6+)', 'TypeScript', 'React', 'Eleventy', 'Node.js', 'WordPress'];
+  const skills = ['JavaScript (ES6+)', 'Python', 'React', 'SQL', 'Node.js', 'Express'];
 
   return (
     <StyledAboutSection id="about" ref={revealContainer}>
@@ -134,34 +134,21 @@ const About = () => {
       <div className="inner">
         <StyledText>
           <div>
+            <p>Hello! My name is Kris and I love to create.</p>
             <p>
-              Hello! My name is Brittany and I enjoy creating things that live on the internet. My
-              interest in web development started back in 2012 when I decided to try editing custom
-              Tumblr themes — turns out hacking together a custom reblog button taught me a lot
-              about HTML &amp; CSS!
+              My interest in web development started back in 2013, when I edited my, What Superhero
+              are you? quiz on MySpace — turns out hacking repacing my full name for Superman's name
+              taught me a lot about HTML &amp; CSS!
             </p>
-
             <p>
-              Fast-forward to today, and I’ve had the privilege of working at{' '}
-              <a href="https://us.mullenlowe.com/">an advertising agency</a>,{' '}
-              <a href="https://starry.com/">a start-up</a>,{' '}
-              <a href="https://www.apple.com/">a huge corporation</a>, and{' '}
-              <a href="https://scout.camd.northeastern.edu/">a student-led design studio</a>. My
-              main focus these days is building accessible, inclusive products and digital
-              experiences at <a href="https://upstatement.com/">Upstatement</a> for a variety of
-              clients.
+              Fast-forward to today, and I’ve had the privilege of studying at{' '}
+              <a href="https://www.appacademy.io/">a respected coding bootcamp</a>, to further my
+              knowledge on the field of technology.
             </p>
-
-            <p>
-              I also recently{' '}
-              <a href="https://www.newline.co/courses/build-a-spotify-connected-app">
-                launched a course
-              </a>{' '}
-              that covers everything you need to build a web app with the Spotify API using Node
-              &amp; React.
-            </p>
-
-            <p>Here are a few technologies I’ve been working with recently:</p>
+            My main focus now to to implement the experiences I have gained, to make an immediate
+            impact as a software engineer at a cutting edge company.
+            <p></p>
+            <p>Here are a few technologies I’ve been working with:</p>
           </div>
 
           <ul className="skills-list">
@@ -173,7 +160,7 @@ const About = () => {
           <div className="wrapper">
             <StaticImage
               className="img"
-              src="../../images/me.jpg"
+              src="../../images/kh.jpg"
               width={500}
               quality={95}
               formats={['AUTO', 'WEBP', 'AVIF']}
